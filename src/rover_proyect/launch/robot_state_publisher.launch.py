a57adfb6931808f3e55975269a687d5ef5793ce3
Marina Antolinez <m.antolinez.2023@alumnos.urjc.es>
2026-05-07 18:00:00 +0200

    proyect-code

---
+++ src/rover_proyect/launch/robot_state_publisher.launch.py
@@ -0,0 +1,39 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import xacro
+
+def generate_launch_description():
+
+    pkg_share = get_package_share_directory('rover_proyect')
+
+    xacro_file = os.path.join(pkg_share, 'robots', 'robot.urdf.xacro')
+    robot_description_config = xacro.process_file(xacro_file)
+    robot_description = {'robot_description': robot_description_config.toxml()}
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[robot_description]
+    )
+
+    joint_state_publisher_gui_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        output='screen'
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        output='screen'
+        # Sin -d, RViz abre con configuración por defecto
+    )
+
+    return LaunchDescription([
+        robot_state_publisher_node,
+        joint_state_publisher_gui_node,
+        rviz_node
+    ])
